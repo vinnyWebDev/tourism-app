6742bc9ad995bb0785c27e35a6bff895772f8b05
vinnyWebDev <vinnywebdev@gmail.com>
2023-06-27 17:33:00 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navigation';
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import Destinations from './components/Destinations';
 import About from './components/About';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Navigation />
+      
       <Routes>
-        <Route path="/" element= {<Home/>}></Route>
-        <Route path="/destinations" element= {<Destinations/>}></Route>
-        <Route path="/about" element= {<About/>}></Route>
-        <Route path="/contact" element= {<Contact/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/destinations" element={<Destinations />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
 
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div>
+            <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50 fixed-bottom">
+                <div class="container text-center">
+                    <small>Copyright &copy; Tour Ireland</small>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
