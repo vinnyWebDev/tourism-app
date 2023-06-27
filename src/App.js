d12fab1274652d2ff02be8ee67e5858290b82b43
vinnyWebDev <vinnywebdev@gmail.com>
2023-06-27 15:11:37 +0100

    added navbat

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarBs from './components/NavbarBs';
+
 
 function App() {
   return (
     <div>
-   
+      <NavbarBs/>
     </div>
   );
 }
---
+++ src/components/NavbarBs.js
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavbarBs() {
+    return (
+        <div>
+            <Navbar bg="dark" variant={"dark"} expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">Tour Ireland</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Destinations</Nav.Link>
+                            <Nav.Link href="#link">About</Nav.Link>
+                            <Nav.Link href="#link">Contact</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+export default NavbarBs
