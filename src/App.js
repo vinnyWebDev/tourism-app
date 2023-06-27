d8d124d3bcdc6d3f22c3d36211bf3cf25b5ce01e
vinnyWebDev <vinnywebdev@gmail.com>
2023-06-27 17:05:52 +0100

    routing added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarBs from './components/NavbarBs';
-
+import {Routes, Route} from "react-router-dom";
+import Home from './components/Home';
+import Destinations from './components/Destinations';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div>
-      <NavbarBs/>
+      <NavbarBs />
+      <Routes>
+        <Route path="/" element= {<Home/>}></Route>
+        <Route path="/destinations" element= {<Destinations/>}></Route>
+        <Route path="/about" element= {<About/>}></Route>
+        <Route path="/contact" element= {<Contact/>}></Route>
+      </Routes>
     </div>
+
   );
 }
 
