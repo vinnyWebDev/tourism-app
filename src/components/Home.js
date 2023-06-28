5cf65e67a18f481fcd0312c796077c8f182217cf
vinnyWebDev <vinnywebdev@gmail.com>
2023-06-28 16:08:54 +0100

    upadted repo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
 import React from "react";
+import "../App.css";
 
 function Home(){
     return(
-        <div>
-            Home
+        <section class="bgimage" id="home">
+        <div class="container-fluid">
+            <div class="row">
+            <div class="hero-text">
+                <h2 class="hero_title">Tour Ireland</h2>
+                <p class="hero_desc">Tour the land of saints, scholars and so much more.</p>
+            </div>
+            </div>
         </div>
+    </section>
     )
 }
 
