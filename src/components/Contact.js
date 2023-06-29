d6e6764e23b0bf7840732830f07abe2982bbbc79
vinnyWebDev <vinnywebdev@gmail.com>
2023-06-29 14:42:52 +0100

    contact submits an object with form data

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,23 +3,27 @@ import {useState} from 'react';
 
 function Contact() {
     
-    const [data, setData] =useState({name:"", email:"", phone:"", message:""});
+    const [data, setData] =useState({name:"", email:"", phone:"", userMsg:""});
 
-    const handleChange = () =>{
+    const handleChange = (e) =>{
+        const name = e.target.name;
+        const value = e.target.value;
+        setData({...data,[name]:value});
 
     }
 
-    const handleSubmit = () =>{
-
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        console.log(data)
     }
 
     return (
         <div>
             <form method="post" onSubmit={handleSubmit}>
-                <input class="inputField" type="text" name="name" id="" onChange={handleChange} value={data.name} placeholder="name"></input>
-                <input class="inputField"  type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder="email"></input>
-                <input  class="inputField" type="phone" name="phone" id="" onChange={handleChange} value={data.phone} placeholder="phone"></input>
-                <textarea class="inputField" name="userMsg" id="" cols="30" rows="10" onChange={handleChange} value={data.message} placeholder="message"></textarea>
+                <input className="inputField" type="text" name="name" id="" onChange={handleChange} value={data.name} placeholder="name"></input>
+                <input className="inputField"  type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder="email"></input>
+                <input  className="inputField" type="phone" name="phone" id="" onChange={handleChange} value={data.phone} placeholder="phone"></input>
+                <textarea className="inputField" name="userMsg" id="" cols="30" rows="10" onChange={handleChange} value={data.message} placeholder="message"></textarea>
                 <button type="submit">Submit</button>
             </form>
         </div>
