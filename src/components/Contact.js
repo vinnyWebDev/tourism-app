df72e6fd8d813463db5fc758a6066058679a4763
vinnyWebDev <vinnywebdev@gmail.com>
2023-06-29 21:31:07 +0100

    final commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,21 +22,22 @@ function Contact() {
     const handleSubmit = (e) =>{
         e.preventDefault()
         console.log(data)
+        
     }
 
     return (
         <div>
             <form method="post" onSubmit={handleSubmit}>
-                <input className="inputField" type="text" name="name" id="" onChange={handleChange} value={data.name} placeholder="name*"></input>
+                <input className="inputField" type="text" name="name"  onChange={handleChange} value={data.name} placeholder="name*"></input>
                 {error&&data.name==0?
                 <label>Name field cannot be blank</label>:""}
-                <input className="inputField"  type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder="email*"></input>
+                <input className="inputField"  type="email" name="email" onChange={handleChange} value={data.email} placeholder="email*"></input>
                 {error&&data.email==0?
                 <label>Email field cannot be blank</label>:""}
-                <input  className="inputField" type="phone" name="phone" id="" onChange={handleChange} value={data.phone} placeholder="phone"></input>
+                <input  className="inputField" type="phone" name="phone"  onChange={handleChange} value={data.phone} placeholder="phone"></input>
                 {error&&data.userMsg==0?
                 <label>Message field cannot be blank</label>:""}
-                <textarea className="inputField" name="userMsg" id="" cols="30" rows="10" onChange={handleChange} value={data.message} placeholder="message"></textarea>
+                <textarea className="inputField" name="userMsg" cols="30" rows="10" onChange={handleChange} value={data.message} placeholder="message"></textarea>
                 {error?
                 <button type="submit" disabled>Submit</button>:<button type="submit">Submit</button>}
             </form>
